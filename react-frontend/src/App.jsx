f94a216e925bf345b98219fbe00467983e450be7
Kevin <57780570+KoopaTroopa23@users.noreply.github.com>
2026-03-24 18:28:55 -0400

    main - Add React frontend with Vite setup

---
+++ react-frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Weather App</h1>
+      <p>My React weather app is ready to build.</p>
+    </div>
+  );
+}
+
+export default App;
